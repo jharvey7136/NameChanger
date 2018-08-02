0dc1a316e1b8b180b04706c70cdce27685ecafd4
jharvey7136 <jharvey7136@mail.kvcc.edu>
2018-08-02 13:48:34 -0400

    init commit

--- StringFormat/Program.cs
+++ StringFormat/Program.cs
@@ -8,8 +8,71 @@ namespace StringFormat
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            string fileName = "names.txt";
+            
+
+            using (var filestream = new System.IO.FileStream(fileName,
+                                        System.IO.FileMode.Open,
+                                        System.IO.FileAccess.Read,
+                                        System.IO.FileShare.ReadWrite))
+            using (var file = new System.IO.StreamReader(filestream, System.Text.Encoding.UTF8))
+            {
+                string line, first, last;
+                int count;
+                List<string> nameList = new List<string>();
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    var words = line.Split(null).Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s));                    
+                    count = words.Count();
+                    Console.WriteLine(count);
+
+                    first = words.ElementAt(0);
+
+                    if (count < 3)
+                    {
+                        last = words.ElementAt(1);
+                    }
+                    else
+                    {
+                        if (words.ElementAt(2) == null || words.ElementAt(3) == null || words.ElementAt(4) == null || words.ElementAt(5) == null)
+                        {
+                            last = words.ElementAt(1);
+                        }
+                        else
+                        {
+                            last = words.ElementAt(2);
+                        }
+                                              
+                    }
+                    
+                    nameList.Add(last + ", " + first);                  
+                    
+                }
+
+                foreach (var name in nameList)
+                {
+                    Console.WriteLine(name);
+                }
+
+                /*
+                using (System.IO.StreamWriter fileOut = new System.IO.StreamWriter("namesOut.txt"))
+                {
+                    foreach (var name in nameList)
+                    {
+                        fileOut.WriteLine(name);
+                    }
+                }  
+                */
+            }
+
+            Console.WriteLine();
+            
+
         }
     }
 }
